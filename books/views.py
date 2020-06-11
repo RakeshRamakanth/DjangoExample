b842211ed6fcd9e4769723453772736f195cc695
Ramakanth, Rakesh <rakesh.ramakanth@aegon.nl>
2020-06-11 19:11:15 +0200

    templates are included

--- books/views.py
+++ books/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-# Create your views here.
+from django.template import loader
 from .models import Books
-
+from django.http import Http404
 
 def index(request):
     all_books = Books.objects.all()
-    html = ''
-    for book in all_books:
-        url = '/books/'+str(book.id)+'/'
-        html += '<a href= "'+url+'">'+ str(book.name) + '</a><br>'
-    return HttpResponse(html)
+    context = {
+        'all_books': all_books
+    }
+    return render(request,'books/index.html',context)
 
 def detail(request, book_id):
-    return HttpResponse("<h2> Details for Book Id"+str(book_id)+"</h2>")
+    try:
+        book = Books.objects.get(id=book_id)
+    except Books.DoesNotExist:
+        raise Http404('This page does not exists')
+    context = {
+        'book': book
+    }
+    return render(request,'books/detail.html',context)
