5c2c713611e4b71eef79112c0db642a656b96db8
Ramakanth <Rakesh.Ramakanth@aegon.nl>
2020-07-05 11:49:08 +0200

    Bootstrap included

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
     url(r'^$',views.index, name= 'index'),
     #/books/1
